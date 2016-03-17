c5c9b9efa71b034f2907d5e5fded594abf7226d6
swelham <stuart.welham@gmail.com>
2016-03-17 16:51:36 +0000

    added CliApp default values

---
+++ SharpCommands/SharpCommands.Tests/CliAppTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpCommands.Tests
+{
+    [TestClass]
+    public class CliAppTests
+    {
+        const string CLI_APP_NAME = "TestApp";
+
+        [TestMethod]
+        public void Should_Have_Default_Values()
+        {
+            var app = new CliApp(CLI_APP_NAME);
+
+            Assert.AreEqual(app.Name, CLI_APP_NAME);
+            Assert.AreEqual(app.Version, "0.0.0.0");
+        }
+    }
+}
---
+++ SharpCommands/SharpCommands/CliApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCommands
+{
+    public class CliApp
+    {
+        public CliApp(string name)
+        {
+            this.Name = name;
+            this.Version = "0.0.0.0";
+        }
+
+        public string Name { get; private set; }
+
+        public string Version { get; set; }
+    }
+}
