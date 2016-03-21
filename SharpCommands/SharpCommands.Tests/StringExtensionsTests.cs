837cd838243f0fa1ac67e1972e01a90fab204d50
stuart.welham@gmail.com <stuart.welham@gmail.com>
2016-03-21 21:59:08 +0000

    added missing IsFlagMatch tests

--- SharpCommands/SharpCommands.Tests/StringExtensionsTests.cs
+++ SharpCommands/SharpCommands.Tests/StringExtensionsTests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpCommands.Text;
+using SharpCommands.Tests.Fixtures.Flags;
 
 namespace SharpCommands.Tests
 {
@@ -24,6 +25,24 @@ namespace SharpCommands.Tests
         public void IsFlag_Should_Return_False_For_Invalid_Flag()
         {
             Assert.IsFalse("invalid".IsFlag());
+        }
+
+        [TestMethod]
+        public void IsFlagMatch_Should_Return_True_For_Matched_Flag()
+        {
+            var flag = new TestFlag();
+
+            Assert.IsTrue("-t".IsFlagMatch(flag));
+            Assert.IsTrue("--test".IsFlagMatch(flag));
+        }
+
+        [TestMethod]
+        public void IsFlagMatch_Should_Return_False_For_Unmatched_Flag()
+        {
+            var flag = new TestFlag();
+
+            Assert.IsFalse("-i".IsFlagMatch(flag));
+            Assert.IsFalse("--invalid".IsFlagMatch(flag));
         }
     }
 }
